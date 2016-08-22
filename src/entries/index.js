43fa4be9e7a4240b782d7eb85842b62e9d573dad
supersoup <819127251@qq.com>
2016-08-22 16:46:17 +0800

    index confict

--- src/entries/index.js
+++ src/entries/index.js
@@ -5,8 +5,7 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import Routes from '../routes/index';
 
-
-
 ReactDOM.render(
   <Routes history={browserHistory} />
   , document.getElementsByTagName('body')[0]);
+
